2e1e5ea2e94ada17748f549bdbed26cfe310e580
pranavgundu <pranav@gundu.me>
2025-10-23 22:46:55 -0400

    more stuff

---
+++ Scanner/PracticeScanner.java
@@ -0,0 +1,16 @@
+package Scanner;
+import kareltherobot.*;
+import java.util.Scanner;
+
+public class PracticeScanner implements Directions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the size of the shape that you would like to be made: ");
+        int size = scanner.nextInt();
+        int worldSize = 2 * (size + 2);
+
+        Robot robot = new Robot(1, worldSize / 2, North, size^2);
+        World.setSize(worldSize, worldSize);
+        World.setVisible(true);
+    }
+}
