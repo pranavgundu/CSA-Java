f26a8317b82e58c2883920b8404e7f5dc1fdcb3a
pranavgundu <pranav@gundu.me>
2025-10-29 20:00:18 -0400

    prac

--- Scanner/PracticeScanner.java
+++ Scanner/PracticeScanner.java
@@ -7,10 +7,15 @@ public class PracticeScanner implements Directions {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter the size of the shape that you would like to be made: ");
         int size = scanner.nextInt();
+        scanner.close();
         int worldSize = 2 * (size + 2);
 
         Robot robot = new Robot(1, worldSize / 2, North, size^2);
         World.setSize(worldSize, worldSize);
         World.setVisible(true);
+
+        int beepersPlaced = 0;
+
+        System.out.println("The shape has been completed using " + beepersPlaced + " beepers.");
     }
 }
